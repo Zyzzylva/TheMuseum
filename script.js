ed31cb1583324ea8863e561a6d04bb76c0fb71d3
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-08 19:12:37 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 fetch("facts.json");
-document.getElementById("factText").textContent = "waaaaa";
+let i = Math.floor(Math.random() * facts.length);
+document.getElementById("factText").textContent = facts[i].factText;
 document.getElementById("factImage").setAttribute("src","https://inaturalist-open-data.s3.amazonaws.com/photos/171151444/large.jpeg");
 document.getElementById("imgCredit").textContent = "this phto is by sloog :)"; 
