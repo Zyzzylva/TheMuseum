044dc9f4ab2b8c6fb9fd66896a30535ad6d1eb1c
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-09 10:54:34 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-fetch("testing.json")
+fetch("facts.json")
     .then(response => response.json())
-    .then(testing => {
-        const element = testing[Math.floor(Math.random() * testing.length)];
-        document.getElementById("imgCredit").textContent = element;
+    .then(facts => {
+        const element = facts[Math.floor(Math.random() * facts.length)];
+        document.getElementById("imgCredit").textContent = element.factText;
+        document.getElementById("factText").textContent = "I have repalced the text";
+        document.getElementById("factImage").setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Three_populations_of_Chrysomallon_squamiferum.jpg/1280px-Three_populations_of_Chrysomallon_squamiferum.jpg");
     })
-    let i = Math.floor(Math.random() * testing.length);
-    document.getElementById("factText").textContent = "I have repalced the text";
-    document.getElementById("factImage").setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Three_populations_of_Chrysomallon_squamiferum.jpg/1280px-Three_populations_of_Chrysomallon_squamiferum.jpg");
-    const testMake = document.createElement("p");
-    testMake.textContent = "i have made and appended an element :)";
-    document.getElementById("header").appendChild(testMake);
