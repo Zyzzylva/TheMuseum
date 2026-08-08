274356e0de224f3617826979346ea977eda8e0ff
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-08 19:22:57 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch("facts.json");
 let i = Math.floor(Math.random() * facts.length);
 document.getElementById("factText").textContent = facts[i].factText;
-document.getElementById("factImage").setAttribute("src","https://inaturalist-open-data.s3.amazonaws.com/photos/171151444/large.jpeg");
-document.getElementById("imgCredit").textContent = "this phto is by sloog :)"; 
+document.getElementById("factImage").setAttribute("src", facts[i].imgsrc);
+document.getElementById("imgCredit").textContent = facts[i].imgCredit; 
