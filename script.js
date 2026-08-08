feb5ce0719c5c9db0c370567123bc6a6fcdb9117
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-09 10:45:30 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const testing =  ["one", "two", "three", "four"];
+fetch("testing.json")
+    .then(response => response.json())
+    .then(testing => {
+        const element = testing[Math.floor(Math.random() * testing.length)];
+        document.getElementById("imgCredit").textContent = element;
+    })
     let i = Math.floor(Math.random() * testing.length);
     document.getElementById("factText").textContent = "I have repalced the text";
     document.getElementById("factImage").setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Three_populations_of_Chrysomallon_squamiferum.jpg/1280px-Three_populations_of_Chrysomallon_squamiferum.jpg");
-    document.getElementById("imgCredit").textContent = testing[i]; 
+    const testMake = document.createElement("p");
+    testMake.textContent = "i have made and appended an element :)";
+    document.getElementById("header").appendChild(testMake);
