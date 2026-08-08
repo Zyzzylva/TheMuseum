914659f14027e618fcdfa40f2b5177c21390053d
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-08 19:44:47 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 fetch("facts.json")
-  .then((response) => response.json());
-let i = Math.floor(Math.random() * facts.length);
-document.getElementById("factText").textContent = "I have repalced the text";
-document.getElementById("factImage").setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Three_populations_of_Chrysomallon_squamiferum.jpg/1280px-Three_populations_of_Chrysomallon_squamiferum.jpg");
-document.getElementById("imgCredit").textContent = facts[i].imgCredit; 
+  .then(response => response.json())
+  .then(facts => {
+    let i = Math.floor(Math.random() * facts.length);
+    document.getElementById("factText").textContent = "I have repalced the text";
+    document.getElementById("factImage").setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Three_populations_of_Chrysomallon_squamiferum.jpg/1280px-Three_populations_of_Chrysomallon_squamiferum.jpg");
+    document.getElementById("imgCredit").textContent = facts[i].imgCredit; 
+  });
