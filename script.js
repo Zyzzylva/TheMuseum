f9be8a2d316c51ac4adbd1e4b1261a10d0e46695
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-08 13:02:09 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-fetch("facts.json")
-  .then((response) => response.json())
-  .then((facts) => {
-    const list = document.querySelector("#starred");
-    facts.forEach((fact) => {
-      const factText = document.createElement("p");
-      factText.textContent = `${fact.fact} — cr ${fact.imgcredit}`;
-      const factImg = document.createElement("img");
-      factImg.setAttribute("imgsrc", fact.imgsrc);
-      list.appendChild(item);
-    });
-  });
+fetch("facts.json");
+document.getElementById("factText").textContent = "waaaaa";
+document.getElementById("factImage").setAttribute("src","https://inaturalist-open-data.s3.amazonaws.com/photos/171151444/large.jpeg");
+document.getElementById("imgCredit").textContent = "this phto is by sloog :)"; 
