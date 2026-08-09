aab248bd896b5065bf7a2de6298b28333ddc3286
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-09 12:57:48 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ fetch("facts.json")
     .then(response => response.json())
     .then(facts => {
         const element = facts[Math.floor(Math.random() * facts.length)];
-        document.getElementById("imgCredit").textContent = element.imgCredit;
-        document.getElementById("factText").innerHTML = `#${element.id}: ${element.factText}`;
         document.getElementById("factImage").setAttribute("src", element.imgsrc);
     });
