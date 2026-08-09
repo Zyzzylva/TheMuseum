1bae6ce9fcb24f291632d6afb6be33d1c3a183af
Zyzzylva <85777656+Zyzzylva@users.noreply.github.com>
2026-08-09 13:11:00 +1200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ fetch("testing.json")
         const element = testing[Math.floor(Math.random() * testing.length)];
         document.getElementById("factImage").setAttribute("src", "");
         document.getElementById("factText").textContent = "i have replaced the text";
+    })
+    .catch(error => {
+        document.getElementById("factText").textContent = "Oh no! Could not fetch the fact :C";
     });
